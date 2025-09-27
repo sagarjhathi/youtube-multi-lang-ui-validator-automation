d0e118ddad6c4fe037b0478e4a0e18ab22acf896
sagarjhathi <sagarjhathii@gmail.com>
2025-09-27 15:55:21 +0530

    Tested the lingua for multiple texts content from yt landing page

--- yt_multi_language_ui_validator/src/yt_multi_language_ui_validator/Basictest.java
+++ yt_multi_language_ui_validator/src/yt_multi_language_ui_validator/Basictest.java
@@ -60,34 +60,98 @@ public class BasicTest {
 //	
 //     	
 //			
-		for(int j=1;j<languageList.size();j++) {
-			
+//		for(int j=1;j<languageList.size();j++) {
+//			
+//		
+//			languageList=	driver.findElements(yt.languageList);		
+//			languageList.removeIf(el -> el.getText().trim().isEmpty());
+//			String langText=languageList.get(j).getText();
+//			System.out.println(langText+"    "+j);
+//			
+//			
+//	     		WebElement test=driver.findElement(yt.getLanguageElementByName(langText));
+//				WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
+//				wait.until(ExpectedConditions.elementToBeClickable(test));
+//				test.click();
+//				Thread.sleep(3000);
+//				List<WebElement> listOfSideMenu=driver.findElements(yt.sideMenuExpandedList);
+//				StringBuilder sb=new StringBuilder();
+//				for(int i=0;i<listOfSideMenu.size();i++) {
+//					System.out.println(listOfSideMenu.get(i).getText());
+//					sb.append(listOfSideMenu.get(i).getText());
+//				}
+//				
+//				LinguaHelper.detectLanguage(sb.toString());
+//				Thread.sleep(2000);				
+//				driver.findElement(yt.settingEllipsesButton).click();
+//				Thread.sleep(2000);
+//				driver.findElement(yt.languageDropdownUnderSettings).click();
+//			}
 		
-			languageList=	driver.findElements(yt.languageList);		
-			languageList.removeIf(el -> el.getText().trim().isEmpty());
-			String langText=languageList.get(j).getText();
-			System.out.println(langText+"    "+j);
+		
+//		
+//		for(int j=1;j<languageList.size();j++) {
+//			
+//			driver.findElement(yt.ellipsesYtLandingPageTopLeft).click();
+//			languageList=	driver.findElements(yt.languageList);		
+//			languageList.removeIf(el -> el.getText().trim().isEmpty());
+//			String langText=languageList.get(j).getText();
+//			System.out.println(langText+"    "+j);
+//			
+//			
+//	     		WebElement test=driver.findElement(yt.getLanguageElementByName(langText));
+//				WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
+//				wait.until(ExpectedConditions.elementToBeClickable(test));
+//				test.click();
+//				Thread.sleep(3000);
+//				List<WebElement> listOfSideMenu=driver.findElements(yt.sideMenuCollapsedList);
+//				StringBuilder sb=new StringBuilder();
+//				for(int i=0;i<listOfSideMenu.size();i++) {
+//					System.out.println(listOfSideMenu.get(i).getText());
+//					sb.append(listOfSideMenu.get(i).getText());
+//				}
+//				
+//				LinguaHelper.detectLanguage(sb.toString());
+//				Thread.sleep(2000);				
+//				driver.findElement(yt.settingEllipsesButton).click();
+//				Thread.sleep(2000);
+//				driver.findElement(yt.languageDropdownUnderSettings).click();
+//				driver.findElement(yt.ellipsesYtLandingPageTopLeft).click();
+//			}
 			
-			
-	     		WebElement test=driver.findElement(yt.getLanguageElementByName(langText));
-				WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
-				wait.until(ExpectedConditions.elementToBeClickable(test));
-				test.click();
-				Thread.sleep(3000);
-				List<WebElement> listOfSideMenu=driver.findElements(yt.sideMenuExpandedList);
-				StringBuilder sb=new StringBuilder();
-				for(int i=0;i<listOfSideMenu.size();i++) {
-					System.out.println(listOfSideMenu.get(i).getText());
-					sb.append(listOfSideMenu.get(i).getText());
-				}
-				
-				LinguaHelper.detectLanguage(sb.toString());
-				Thread.sleep(2000);				
-				driver.findElement(yt.settingEllipsesButton).click();
-				Thread.sleep(2000);
-				driver.findElement(yt.languageDropdownUnderSettings).click();
-			}
-			
+     	
+     	
+		for(int j=1;j<languageList.size();j++) {
+		
+	
+		languageList=	driver.findElements(yt.languageList);		
+		languageList.removeIf(el -> el.getText().trim().isEmpty());
+		String langText=languageList.get(j).getText();
+		System.out.println(langText+"    "+j);
+		
+		
+     		WebElement test=driver.findElement(yt.getLanguageElementByName(langText));
+//			WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
+//			wait.until(ExpectedConditions.elementToBeClickable(test));
+     		
+			test.click();
+			Thread.sleep(3000);
+			driver.findElement(yt.settingEllipsesButton).click();
+//			List<WebElement> listOfSideMenu=driver.findElements(yt.settingEllipsesOptionsListLandingPage);
+//			StringBuilder sb=new StringBuilder();
+//			for(int i=0;i<listOfSideMenu.size();i++) {
+//				System.out.println(listOfSideMenu.get(i).getText());
+//				sb.append(listOfSideMenu.get(i).getText());
+//			}
+			Thread.sleep(2000);
+			String ellipsesOption=driver.findElement(yt.settingEllipsesOptionsListLandingPage).getText();
+			System.out.println(ellipsesOption);
+			LinguaHelper.detectLanguage(ellipsesOption);
+			Thread.sleep(2000);				
+			//driver.findElement(yt.settingEllipsesButton).click();
+		//	Thread.sleep(2000);
+			driver.findElement(yt.languageDropdownUnderSettings).click();
+		}
 		
 		
 		
