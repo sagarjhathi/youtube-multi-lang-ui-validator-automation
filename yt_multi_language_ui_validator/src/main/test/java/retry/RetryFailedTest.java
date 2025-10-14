510e0fddbbc74e51e4d5a3c8e4bdb7379208f765
sagarjhathi <sagarjhathii@gmail.com>
2025-10-14 18:02:27 +0530

    Added logs to the retry analyzer class

--- yt_multi_language_ui_validator/src/main/test/java/retry/RetryFailedTest.java
+++ yt_multi_language_ui_validator/src/main/test/java/retry/RetryFailedTest.java
@@ -1,5 +1,6 @@
 package main.test.java.retry;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.testng.IRetryAnalyzer; 
 import org.testng.ITestResult;
 
@@ -26,14 +27,23 @@ public class RetryFailedTest implements IRetryAnalyzer {
         if (retryCount < maxRetryCount && (cause == null || isRetryable(cause))) {
             retryCount++;
             System.out.println("Retrying test: " + result.getName() + " due to " + cause);
+            log.warn("[{}] Retrying test '{}' (attempt {}) due to: {}", 
+                    ThreadContext.get("testName"), result.getName(), retryCount, cause != null ? cause.getMessage() : "Unknown error");
+          
             return true;
         }
+        
+        log.info("[{}] Not retrying test '{}'. Retry count: {}, Cause: {}", 
+                ThreadContext.get("testName"), result.getName(), retryCount, cause != null ? cause.getMessage() : "None");
+        
         return false;
     }
 
     private boolean isRetryable(Throwable cause) {
         for (Class<? extends Throwable> nonRetryable : nonRetryableExceptions) {
             if (nonRetryable.isInstance(cause)) {
+                log.debug("[{}] Cause '{}' marked as non-retryable", ThreadContext.get("testName"), cause.getClass().getSimpleName());
+
                 return false;
             }
         }
