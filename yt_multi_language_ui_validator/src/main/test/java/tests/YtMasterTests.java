f30e8930ecd1fc9c138919fb105154caefa11fa9
sagarjhathi <sagarjhathii@gmail.com>
2025-12-18 22:49:44 +0530

    Formatting the code for side Menu Normal , lingua Tests

--- yt_multi_language_ui_validator/src/main/test/java/tests/YtMasterTests.java
+++ yt_multi_language_ui_validator/src/main/test/java/tests/YtMasterTests.java
@@ -30,8 +30,7 @@ public class YtMasterTests extends BaseTest{
 
 		
 		
-		YtLandingPage yt=new YtLandingPage();
-		GenericUtility g=new GenericUtility();
+		YtLandingPage landingPage=new YtLandingPage();
 		SoftAssert softAssert = new SoftAssert();
 		
 		
@@ -45,49 +44,49 @@ public class YtMasterTests extends BaseTest{
 		verifyingSideMenuLanguageAsInSettingsDataReader.loadSheet("verifyingSideMenuLanguageAsInSe");
 
 		
-		yt.openingLandingPage();
-		yt.clickingSettingEllipsesButton();
+		landingPage.openingLandingPage();
+		landingPage.clickingSettingEllipsesButton();
 		Thread.sleep(2000);
-		yt.clickingLanguageDropdownButton();
+		landingPage.clickingLanguageDropdownButton();
 		Thread.sleep(2000);
 		String testName = ThreadContext.get("logFileName");
 		int LanguagesRowCount= reader.getRowCount();
 
 
-		for(int j=1;j<LanguagesRowCount;j++) {
+		for(int languageIndex=1;languageIndex<LanguagesRowCount;languageIndex++) {
 
-			String langText=reader.getCellValue(j, 0);
-			System.out.println(langText+" lang text from the sheet   "+j);
+			String langText=reader.getCellValue(languageIndex, 0);
+			System.out.println(langText+" lang text from the sheet   "+languageIndex);
 			
 			ScreenshotUtil.capture(testName, langText);
 			Thread.sleep(1000);
 			
-			yt.getLanguageElementByName(langText).click();	
+			landingPage.getLanguageElementByName(langText).click();	
 
 			Thread.sleep(2000);
-			List<WebElement> listOfSideMenu=yt.gettingSideMenuExpandedList();
+			List<WebElement> listOfSideMenu=landingPage.gettingSideMenuExpandedList();
 
-			StringBuilder sb=new StringBuilder();
-			for(int i=0;i<listOfSideMenu.size();i++) {
-				System.out.println(listOfSideMenu.get(i).getText());
-				sb.append(listOfSideMenu.get(i).getText());
-				if(i!=listOfSideMenu.size()-1) {
-					sb.append(" ");
+			StringBuilder sideMenuItems=new StringBuilder();
+			for(int sideMenuItem=0;sideMenuItem<listOfSideMenu.size();sideMenuItem++) {
+				System.out.println(listOfSideMenu.get(sideMenuItem).getText());
+				sideMenuItems.append(listOfSideMenu.get(sideMenuItem).getText());
+				if(sideMenuItem!=listOfSideMenu.size()-1) {
+					sideMenuItems.append(" ");
 				}
 			
 			}
 			
 			
 			
-			String expectedData=verifyingSideMenuLanguageAsInSettingsDataReader.getCellValue(j, 1);
-			String actualData=sb.toString();
+			String expectedData=verifyingSideMenuLanguageAsInSettingsDataReader.getCellValue(languageIndex, 1);
+			String actualData=sideMenuItems.toString();
 			
 			System.out.println("========="+expectedData+" =====  Expected data from the sheet");
 			softAssert.assertEquals(actualData,expectedData,"Mismatch in the Actual and Expected data");
 
-			yt.clickingSettingEllipsesButton();
+			landingPage.clickingSettingEllipsesButton();
 			Thread.sleep(2000);
-			yt.clickingLanguageDropdownButton();
+			landingPage.clickingLanguageDropdownButton();
 			Thread.sleep(2000);
 
 		}
@@ -110,8 +109,8 @@ public class YtMasterTests extends BaseTest{
 
 		
 		
-		YtLandingPage yt=new YtLandingPage();
-		GenericUtility g=new GenericUtility();
+		YtLandingPage landingPage=new YtLandingPage();
+		GenericUtility genericUtility=new GenericUtility();
 		SoftAssert softAssert = new SoftAssert();
 		
 		
@@ -119,70 +118,59 @@ public class YtMasterTests extends BaseTest{
 		reader.loadWorkbook("data/LanguagesList.xlsx");
 		reader.loadSheet("LanguagesList");
 		
-        
-//		FileReader verifyingSideMenuLanguageAsInSettingsDataReader=new FileReader();
-//		verifyingSideMenuLanguageAsInSettingsDataReader.loadWorkbook("data/verifyingSideMenuLanguageAsInSettings.xlsx");
-//		verifyingSideMenuLanguageAsInSettingsDataReader.loadSheet("verifyingSideMenuLanguageAsInSe");
-
-		
-		yt.openingLandingPage();
-		yt.clickingSettingEllipsesButton();
+       		
+		landingPage.openingLandingPage();
+		landingPage.clickingSettingEllipsesButton();
 		Thread.sleep(2000);
-		yt.clickingLanguageDropdownButton();
+		landingPage.clickingLanguageDropdownButton();
 		Thread.sleep(2000);
 		String testName = ThreadContext.get("logFileName");
 		int LanguagesRowCount= reader.getRowCount();
 
 
-		for(int j=1;j<LanguagesRowCount;j++) {
+		for(int languageIndex=1;languageIndex<LanguagesRowCount;languageIndex++) {
 
-			String langText=reader.getCellValue(j, 0);
-			System.out.println(langText+" lang text from the sheet   "+j);
+			String langText=reader.getCellValue(languageIndex, 0);
+			System.out.println(langText+" lang text from the sheet   "+languageIndex);
 			
 			ScreenshotUtil.capture(testName, langText);
 			Thread.sleep(1000);
 			
-			yt.getLanguageElementByName(langText).click();	
+			landingPage.getLanguageElementByName(langText).click();	
 
 			Thread.sleep(2000);
-			List<WebElement> listOfSideMenu=yt.gettingSideMenuExpandedList();
+			List<WebElement> listOfSideMenu=landingPage.gettingSideMenuExpandedList();
 
-			StringBuilder sb=new StringBuilder();
-			for(int i=0;i<listOfSideMenu.size();i++) {
-				System.out.println(listOfSideMenu.get(i).getText());
-				sb.append(listOfSideMenu.get(i).getText());
-				if(i!=listOfSideMenu.size()-1) {
-					sb.append(" ");
+			StringBuilder sideMenuItems=new StringBuilder();
+			for(int sideMenuItem=0;sideMenuItem<listOfSideMenu.size();sideMenuItem++) {
+				System.out.println(listOfSideMenu.get(sideMenuItem).getText());
+				sideMenuItems.append(listOfSideMenu.get(sideMenuItem).getText());
+				if(sideMenuItem!=listOfSideMenu.size()-1) {
+					sideMenuItems.append(" ");
 				}
 			
 			}
 			
 			
+		
 			
-//			String expectedData=verifyingSideMenuLanguageAsInSettingsDataReader.getCellValue(j, 1);
-//			String actualData=sb.toString();
-			
-	//		System.out.println(expectedData+"   Expected data from the sheet");
-			
-			
-			String applicableLanguage=reader.getCellValue(j, 0);
-			String detectedLanguage=LinguaHelper.detectLanguage(sb.toString());
-			String expectedLanguage=g.getExpectedLangageViaApplicableLangInput(applicableLanguage);
+			String applicableLanguage=reader.getCellValue(languageIndex, 0);
+			String detectedLanguage=LinguaHelper.detectLanguage(sideMenuItems.toString());
+			String expectedLanguage=genericUtility.getExpectedLangageViaApplicableLangInput(applicableLanguage);
 
-			String expectedLanguageAttribute=g.getLangAttributeViaLanguageInput(applicableLanguage);
-			String detectedLanguageAttribute=g.getLangAttribute();
+			String expectedLanguageAttribute=genericUtility.getLangAttributeViaLanguageInput(applicableLanguage);
+			String detectedLanguageAttribute=genericUtility.getLangAttribute();
 
 			System.out.println("ApplicableLanguage  "+applicableLanguage+"     "+"Detected Language   "+detectedLanguage+"  "+"Expected Language  "+expectedLanguage);
 			System.out.println("ApplicableLanguage  "+applicableLanguage+"     "+"Detected Language attribute   "+detectedLanguageAttribute+"  "+"Expected Language attribute "+expectedLanguageAttribute);
 
-		//	softAssert.assertEquals(actualData,expectedData,"Mismatch in the Actual and Expected data");
 			softAssert.assertEquals(detectedLanguage, expectedLanguage, "Page title mismatch");
 			softAssert.assertEquals(detectedLanguageAttribute, expectedLanguageAttribute, "Language attributr mismatch");
 
 
-			yt.clickingSettingEllipsesButton();
+			landingPage.clickingSettingEllipsesButton();
 			Thread.sleep(2000);
-			yt.clickingLanguageDropdownButton();
+			landingPage.clickingLanguageDropdownButton();
 			Thread.sleep(2000);
 
 		}
@@ -265,26 +253,7 @@ public class YtMasterTests extends BaseTest{
 		  
 		  softAssert.assertEquals(actualData, expectedData, "Mismatch in the Expected and actual Data");
 			
-
-//			String applicableLanguage=reader.getCellValue(j, 0);
-//			String detectedLanguage=LinguaHelper.detectLanguage(sb.toString());
-//			log.info("text sent to lingua is "+"      "+sb.toString());
-//			String expectedLanguage=applicableExpectedReader.getCellValue(j, 1);
 		
-
-			
-//			String expectedLanguageAttribute=gn.getLangAttributeViaLanguageInput(applicableLanguage);
-//			String detectedLanguageAttribute=gn.getLangAttribute();
-//
-//			System.out.println("ApplicableLanguage  "+applicableLanguage+"     "+"Detected Language   "+detectedLanguage+"  "+"Expected Language  "+expectedLanguage);
-//			System.out.println("ApplicableLanguage  "+applicableLanguage+"     "+"Detected Language attribute   "+detectedLanguageAttribute+"  "+"Expected Language attribute "+expectedLanguageAttribute);
-//
-//			
-		
-//			softAssert.assertEquals(detectedLanguage, expectedLanguage, "Language detection mismatch");
-//			softAssert.assertEquals(detectedLanguageAttribute, expectedLanguageAttribute, "Language attributr mismatch");
-//
-//			
 			yt.clickingSettingEllipsesButton();
 			Thread.sleep(2000);
 			yt.clickingLanguageDropdownButton();
@@ -582,20 +551,6 @@ public class YtMasterTests extends BaseTest{
 			
 			softAssert.assertEquals(actualValue, expectedData, "Mismatch in the Expected and Actual Data");
 			
-//			String applicableLanguage=reader.getCellValue(j, 0);
-//			String detectedLanguage=LinguaHelper.detectLanguage(str);
-//			String expectedLanguage=gn.getExpectedLangageViaApplicableLangInput(applicableLanguage);
-//
-//
-//			String expectedLanguageAttribute=gn.getLangAttributeViaLanguageInput(applicableLanguage);
-//			String detectedLanguageAttribute=gn.getLangAttribute();
-//
-//			System.out.println("ApplicableLanguage  "+applicableLanguage+"     "+"Detected Language   "+detectedLanguage+"  "+"Expected Language  "+expectedLanguage);
-//			System.out.println("ApplicableLanguage  "+applicableLanguage+"     "+"Detected Language attribute   "+detectedLanguageAttribute+"  "+"Expected Language attribute "+expectedLanguageAttribute);
-//
-//			softAssert.assertEquals(detectedLanguage, expectedLanguage, "Language detection mismatch");
-//			softAssert.assertEquals(detectedLanguageAttribute, expectedLanguageAttribute, "Language attributr mismatch");
-
 
 
 		}
@@ -617,20 +572,20 @@ public class YtMasterTests extends BaseTest{
 	@Test(retryAnalyzer = RetryFailedTest.class)
 	public void verifyingGlobalFilterLandingPageLingua() throws InterruptedException {
 
-		FileReader reader=new FileReader();
+		
 		GenericUtility gn=new GenericUtility();
 		YtLandingPage yt=new YtLandingPage();
 		SoftAssert softAssert =new SoftAssert();
 		
 		
+		FileReader reader=new FileReader();
 		reader.loadWorkbook("data/LanguagesList.xlsx");
 		reader.loadSheet("LanguagesList");
 		int LanguagesRowCount= reader.getRowCount();
 
 		
 		
-	
-		
+
 		yt.openingLandingPage();
 		yt.givingInputUnderSearchBar("video");
 		gn.clickEnter(yt.searchInputLandinfPage);
