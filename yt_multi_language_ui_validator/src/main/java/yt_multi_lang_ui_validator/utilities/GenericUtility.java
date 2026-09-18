61cb689bf9749f6079a842475fac0e14ea962351
sagarjhathi <sagarjhathii@gmail.com>
2026-09-18 22:25:58 +0530

    mimicing cron to see if runs all lang in tests

--- yt_multi_language_ui_validator/src/main/java/yt_multi_lang_ui_validator/utilities/GenericUtility.java
+++ yt_multi_language_ui_validator/src/main/java/yt_multi_lang_ui_validator/utilities/GenericUtility.java
@@ -145,7 +145,8 @@ public class GenericUtility extends BasePage {
 
 	/** Row-count override for the language-driven tests (masterdata.properties: runForAllLanguages / overideLanguageCount*). */
 	public int resolveLanguageIterationCount(int fullRowCount) {
-    	boolean isCron = Boolean.parseBoolean(System.getenv("IS_CRON"));
+    	// boolean isCron = Boolean.parseBoolean(System.getenv("IS_CRON"));
+		boolean isCron = true;
 		
 		String suffix = isCron ? "CI" : "";
 
@@ -158,7 +159,8 @@ public class GenericUtility extends BasePage {
 
 	/** Row-count override for the country-driven tests (masterdata.properties: runForAllCountries / overideCountriesCount*). */
 	public int resolveCountryIterationCount(int fullRowCount) {
-		boolean isCron = Boolean.parseBoolean(System.getenv("IS_CRON"));
+	//	boolean isCron = Boolean.parseBoolean(System.getenv("IS_CRON"));
+	boolean isCron = true;
 		String suffix = isCron ? "CI" : "";
 
 		boolean runForAllCountries = ConfigManager.getBoolean("runForAllCountries" + suffix, false);
